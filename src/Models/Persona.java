9ff98684171ec54413e5facff97cb45ff6e92953
Andrea <2007andreahurtado@gmail.com>
2025-04-22 09:21:21 -0500

    repaso

--- src/Models/Persona.java
+++ src/Models/Persona.java
@@ -6,8 +6,8 @@ package Models;
  */
 public class Persona {
     // Atributos de la clase
-    String nombre; // Almacena el nombre de la persona
-    int edad; // Almacena la edad de la persona
+    private String nombre; // Almacena el nombre de la persona
+    private int edad; // Almacena la edad de la persona // le hacemos privado 
 
     /**
      * Constructor para crear una nueva instancia de Persona con un nombre y edad
@@ -30,6 +30,22 @@ public class Persona {
     @Override
     public String toString() {
         return nombre + " - " + edad; // Devuelve el nombre y la edad de la persona en formato String
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
     }
 
     // TODO: Agregar getters y setters para los atributos de la clase
